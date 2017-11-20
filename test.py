f35c9f60e26fd78866af6b02fd6e8351cc74b49c
Vladiverev <vladiverev@gmail.com>
2017-11-20 21:06:42 +0200

    20.11

--- test.py
+++ test.py
@@ -1,11 +1,4 @@
-from os import path
+from time import time
 
-import json
 
-notebook = dict(brand='Lenovo', size=13, ram='8gb')
-
-with open('les6_1.json', 'a') as file:
-    json.dump(notebook, file)
-
-dumped = json.dumps(notebook)
-print(dumped)
+print(time())
